36b38a169b958339c1e6d8b4c5ec862fe2ac6992
yurii-kovalchuk <sogdrums@gmail.com>
2023-12-03 23:44:28 +0200

    add FAQ section, markup, logic and styles

--- src/sections/Questions/Questions.jsx
+++ src/sections/Questions/Questions.jsx
@@ -1,5 +1,73 @@
+import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import questionData from "../../assets/data/FAQ.json";
+import MinusIcon from "../../assets/media/icons/minus.svg?react";
+import PlusIcon from "../../assets/media/icons/plus.svg?react";
+import ArrowDownIcon from "../../assets/media/icons/arr-down.svg?react";
+import {
+  Answer,
+  Question,
+  QuestItem,
+  QuestTitle,
+  ContactUsLink,
+  ContactUsTxt,
+  ContactUsWrap,
+  QuestList,
+  QuestContainer,
+  QuestCustom,
+  ContactUsQuest,
+} from "./Questions.styled";
+import scroolToSection from "../../utils/scrollToSection";
+
 const Questions = () => {
-  return <div id="faq">FAQ</div>;
+  const [selected, setSelected] = useState(0);
+  const isTablet = useMediaQuery({ query: "(min-width: 768px)" });
+
+  const handleClick = (id, evt) => {
+    evt.preventDefault();
+    scroolToSection(id);
+  };
+
+  return (
+    <section id="faq">
+      <QuestContainer>
+        {!isTablet && <QuestTitle>Frequently Asked Questions</QuestTitle>}
+
+        <QuestList>
+          {questionData.map((data, idx) => {
+            const isActive = selected === idx;
+            return (
+              <QuestItem key={idx}>
+                <Question onClick={() => setSelected(idx)}>
+                  {isActive ? <MinusIcon /> : <PlusIcon />}
+                  <h4>{data.question}</h4>
+                </Question>
+                <Answer $isActive={isActive}>
+                  <p>{data.answer}</p>
+                </Answer>
+              </QuestItem>
+            );
+          })}
+        </QuestList>
+        <QuestCustom>
+          {isTablet && <QuestTitle>Frequently Asked Questions</QuestTitle>}
+
+          <ContactUsWrap>
+            <ContactUsQuest>
+              Didn&apos;t find the answer to your question?
+            </ContactUsQuest>
+            <ContactUsLink
+              href="/"
+              onClick={(evt) => handleClick("contact-us", evt)}
+            >
+              <ContactUsTxt>Contact Us</ContactUsTxt>
+              <ArrowDownIcon />
+            </ContactUsLink>
+          </ContactUsWrap>
+        </QuestCustom>
+      </QuestContainer>
+    </section>
+  );
 };
 
 export default Questions;
---
+++ src/sections/Questions/Questions.styled.jsx
@@ -0,0 +1,208 @@
+import styled, { keyframes, css } from "styled-components";
+import Container from "../../components/Container/Container";
+
+export const QuestContainer = styled(Container)`
+  @media screen and (min-width: 768px) {
+    display: flex;
+    gap: 24px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    justify-content: space-between;
+  }
+`;
+
+export const QuestTitle = styled.h2`
+  margin-bottom: 24px;
+  box-sizing: border-box;
+
+  color: var(--dark-green-btn-color);
+  font-family: var(--main-font);
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 28px;
+  text-transform: uppercase;
+
+  @media screen and (min-width: 768px) {
+    font-size: 36px;
+    line-height: 36px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 48px;
+    line-height: 48px;
+  }
+`;
+
+export const QuestCustom = styled.div`
+  @media screen and (min-width: 768px) {
+    width: var(--tablet-width);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: var(--desktop-width);
+  }
+`;
+
+export const QuestList = styled.ul`
+  @media screen and (min-width: 768px) {
+    width: var(--tablet-width);
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: var(--desktop-width-half);
+  }
+`;
+
+export const QuestItem = styled.li`
+  padding: 16px 0 16px 24px;
+  &:last-child {
+    padding-bottom: 0;
+  }
+  border-top: 1px solid var(--green-btn-color);
+
+  @media screen and (min-width: 768px) {
+    padding-left: 44px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    padding: 24px 0 24px 52px;
+  }
+`;
+
+export const Question = styled.div`
+  position: relative;
+
+  color: var(--dark-green-btn-color);
+  text-align: justify;
+  font-family: var(--second-font);
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.72px;
+
+  svg {
+    position: absolute;
+    left: -8px;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+
+    transform: translateX(-100%);
+  }
+
+  @media screen and (min-width: 768px) {
+    svg {
+      width: 28px;
+      height: 28px;
+      left: -16px;
+      path {
+        stroke-width: 1;
+      }
+    }
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 24px;
+    svg {
+      left: -24px;
+    }
+  }
+`;
+
+export const Answer = styled.div`
+  overflow: hidden;
+  height: ${(props) => (props.$isActive ? "auto" : "0")};
+  margin-top: ${(props) => (props.$isActive ? "16px" : "0")};
+
+  ${({ $isActive }) =>
+    $isActive &&
+    css`
+      animation: ${fadeIn} 1.2s linear 1;
+    `}
+
+  color: var(--dark-green-btn-color);
+  text-align: justify;
+  font-family: var(--second-font);
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.56px;
+
+  @media screen and (min-width: 1280px) {
+    margin-top: ${(props) => (props.$isActive ? "24px" : "0")};
+    font-size: 16px;
+  }
+`;
+
+const fadeIn = keyframes`
+  from {
+    max-height: 0;
+  }
+  to {
+    max-height: 1000px;
+  }
+`;
+
+export const ContactUsWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: spacce-between;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const ContactUsQuest = styled.span`
+  color: var(--dark-green-btn-color);
+  font-family: var(--second-font);
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.72px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 24px;
+  }
+`;
+
+export const ContactUsLink = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+  padding: 10px 16px;
+  border-radius: 500px;
+  background: var(--green-btn-color);
+  color: var(--dark-green-btn-color);
+
+  transition: background 0.3s ease, color 0.3s ease;
+
+  svg circle {
+    fill: var(--dark-green-btn-color);
+    transition: fill 0.3s ease;
+  }
+
+  &:hover {
+    background: var(--dark-green-btn-color);
+    color: var(--green-btn-color);
+    svg circle {
+      fill: var(--green-btn-color);
+    }
+  }
+`;
+
+export const ContactUsTxt = styled.span`
+  font-family: var(--second-font);
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.64px;
+`;
